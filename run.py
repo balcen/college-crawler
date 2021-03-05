849c390884f7edd21938b9df80beb4fc3a9b541d
baicen <james_lu8044@gmail.com>
2021-03-05 15:29:30 +0800

    add: UsNews crawler

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from CollegeData import CollegeData
 from rich.console import Console
+from UsNews import UsNews
 import file
 import time
 import json
@@ -7,22 +8,29 @@ import logging
 
 
 try:
-
     start = time.time()
 
-    college = CollegeData()
-    schools = college.get_total_school_data()
-    file.store(schools)
+    deter = input('college_data/us_news/all?')
 
-    with open('/mnt/c/Users/baicen/Desktop/schools.json', 'w') as outfile:
-        json.dump(schools, outfile)
-    with open('obj/schools.json', 'w') as outfile:
-        json.dump(schools, outfile)
+    if deter == 'college_data' or deter == 'all':
+        college = CollegeData()
+        schools = college.get_total_school_data()
+        file.store(schools)
+
+        with open('/mnt/c/Users/baicen/Desktop/schools.json', 'w') as outfile:
+            json.dump(schools, outfile)
+        with open('obj/schools.json', 'w') as outfile:
+            json.dump(schools, outfile)
+
+    if deter == 'us_news' or deter == 'all':
+        us_news = UsNews()
+        us_news.get_data()
 
     end = time.time()
     print("+++++++++++++ Total +++++++++++++")
     print(end - start)
     print("+++++++++++++++++++++++++++++++++")
+
 except Exception as e:
     console = Console()
     console.print(repr(e), style="bold red")
--- test.py
+++ test.py
@@ -1,33 +1,5 @@
-import database
-import json
-import file
+from UsNews import UsNews
 
+us_news = UsNews()
 
-schools = database.select("schools")
-
-new_schools = list()
-for school in schools:
-    tmp = {
-        "name": json.dumps({
-            "en": school["name_en"],
-            "tw": school["name_tw"],
-            "cn": school["name_cn"],
-        }),
-        "overview": school["overview"],
-        "website": school["website"]
-    }
-
-    new_schools.append(tmp)
-
-old_schools = file.read()
-
-index = 0
-for slug, old_school in old_schools.items():
-    if "name_en" in old_school:
-        del old_school["name_en"]
-    old_school.update({
-        "overview": new_schools[index]
-    })
-    index += 1
-
-file.store(old_schools, "trans-schools")
+us_news.get_data()
